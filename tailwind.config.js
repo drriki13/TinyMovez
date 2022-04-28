7c7ad796633de163622c223d3d869b057cf773e5
Александр Бессонов <dr.riki13@yandex.ru>
2022-04-28 16:51:16 +0300

    init

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'black': '#16181E',
+        'second': '#21242D',
+        'primary': '#00B9AE',
+        'white': '#F9F9F9'
+      }
+    },
+  },
+  plugins: [],
+}
